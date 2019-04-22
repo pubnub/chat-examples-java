c798f64b4b456746e1b854ffa5d95fd9d79921bd
QSD_m <info@qsdbih.com>
2019-04-22 16:03:01 +0200

    Prepare snippets for authorization

--- app/snippets/chatresourcecenter/AuthorizationTest.java
+++ app/snippets/chatresourcecenter/AuthorizationTest.java
@@ -7,35 +7,57 @@ public class AuthorizationTest extends TestHarness {
     @Test
     public void testSettingUpSecureChannels() {
         // tag::PAM-1[]
-        // in progress
+        // in progress PAM-1
         // end::PAM-1[]
     }
 
     @Test
     public void testGrantingReadWriteAccessForUsers() {
         // tag::PAM-2[]
-        // in progress
+        // in progress PAM-2
         // end::PAM-2[]
     }
 
     @Test
     public void testExtendingAccessForUsers() {
         // tag::PAM-3[]
-        // in progress
+        // in progress PAM-3
         // end::PAM-3[]
     }
 
     @Test
     public void testRevokingAccessForUsers() {
         // tag::PAM-4[]
-        // in progress
+        // in progress PAM-4
         // end::PAM-4[]
     }
 
     @Test
     public void testBanningAndKickingUsers() {
         // tag::PAM-5[]
-        // in progress
+        // in progress PAM-5
         // end::PAM-5[]
     }
+
+    @Test
+    public void test6() {
+        // tag::PAM-6[]
+        // in progress PAM-6
+        // end::PAM-6[]
+    }
+
+    @Test
+    public void test7() {
+        // tag::PAM-7[]
+        // in progress PAM-7
+        // end::PAM-7[]
+    }
+
+    @Test
+    public void test8() {
+        // tag::PAM-8[]
+        // in progress PAM-8
+        // end::PAM-8[]
+    }
+
 }
